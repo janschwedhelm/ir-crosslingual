28b1d5e04deaff00f57784ae6f7d8e370cb94901
Himalayancat <58749052+Himalayancat@users.noreply.github.com>
2020-04-04 12:29:40 +0200

    Delete europarldata_loading.py

--- induce_multilingual_embedding_space/europarldata_loading.py
+++
@@ -1,9 +0,0 @@
-import wget
-import gzip
-
-file_url = 'http://opus.nlpl.eu/download.php?f=Europarl/v8/tmx/de-en.tmx.gz'
-wget.download(file_url, 'de-en.tmx.gz')
-
-import shutil
-with gzip.open('de-en.tmx.gz', 'rb') as f_in, open('de-en.txt', 'wb') as f_out:
-  shutil.copyfileobj(f_in, f_out)
