8eaa1ad88d2274b8582da67339cb103e0a700625
Rebecca Armbruster <rebeca.armbruster@sap.com>
2020-04-25 09:04:59 +0200

    Update cosine similarity to single source sentence computation

--- ir_crosslingual/features/vector_based.py
+++ ir_crosslingual/features/vector_based.py
@@ -1,48 +1,55 @@
+import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.spatial import distance
 from scipy.stats import wasserstein_distance
 
 
-def cosine_sim(s_vec, t_vec):
+# Updated functions
+def cos_sim(src_vec, trg_vec, single_source=False):
     """
-    Computes cosine similarity between the embeddings of two sentences
-    :param s_vec: Sentence embedding of the source sentence
-    :param t_vec: Sentence embedding of the target sentence
+    Computes cosine similarity between the embeddings of two supervised_classification
+    :param src_vec: Sentence embedding of the source sentence
+    :param trg_vec: Sentence embedding of the target sentence
+    :param single_source: True if only one source sentence is compared to multiple target sentences
+    (can be used when ranking target sentences for a single source sentence)
     :return: Cosine similarity of the source and the target sentence embedding
     """
-    cos_sim = cosine_similarity(s_vec.reshape(1,-1), t_vec.reshape(1,-1))
-    return cos_sim[0][0]
+    if single_source:
+        return cosine_similarity(src_vec.reshape(1, -1), np.vstack(trg_vec))[0]
+    else:
+        return cosine_similarity(list(src_vec), list(trg_vec)).diagonal()
 
 
-def euclidean_dist(s_vec, t_vec):
+# TODO: Old functions that need to adapted to new structure
+def euclidean_dist(src_vec, trg_vec):
     """
-    Computes euclidean distance between the embeddings of two sentences
-    :param s_vec: Sentence embedding of the source sentence
-    :param t_vec: Sentence embedding of the target sentence
+    Computes euclidean distance between the embeddings of two supervised_classification
+    :param src_vec: Sentence embedding of the source sentence
+    :param trg_vec: Sentence embedding of the target sentence
     :return: Euclidean distance of the source and the target sentence embedding
     """
-    return distance.euclidean(s_vec, t_vec)
+    return distance.euclidean(src_vec, trg_vec)
 
 
-def jenson_shannon_dist(s_vec, t_vec):
+def jenson_shannon_dist(src_vec, trg_vec):
     """
-    Computes jenson shannon distance between the embeddings of two sentences
-    :param s_vec: Sentence embedding of the source sentence
-    :param t_vec: Sentence embedding of the target sentence
+    Computes jenson shannon distance between the embeddings of two supervised_classification
+    :param src_vec: Sentence embedding of the source sentence
+    :param trg_vec: Sentence embedding of the target sentence
     :return: Jenson-shannon distance of the source and the target sentence embedding
     """
     # TODO: Not working yet, currently returns inf for all vectors in the dataframe
-    return distance.jensenshannon(s_vec, t_vec)
+    return distance.jensenshannon(src_vec, trg_vec)
 
 
-def wasserstein_dist(s_vec, t_vec):
+def wasserstein_dist(src_vec, trg_vec):
     """
-    Computes wasserstein distance between the embeddings of two sentences
-    :param s_vec: Sentence embedding of the source sentence
-    :param t_vec: Sentence embedding of the target sentence
+    Computes wasserstein distance between the embeddings of two supervised_classification
+    :param src_vec: Sentence embedding of the source sentence
+    :param trg_vec: Sentence embedding of the target sentence
     :return: Wasserstein distance of the source and the target sentence embedding
     """
-    return wasserstein_distance(s_vec, t_vec)
+    return wasserstein_distance(src_vec, trg_vec)
 
 
 def most_similar(initial_word: str, words2compare: list):
@@ -57,36 +64,43 @@ def most_similar(initial_word: str, words2compare: list):
     # IDEA: Maybe save similarity values that have been computed before?
     max_sim = ('', 0.0)
     for word in words2compare:
-        tmp_sim = cosine_sim(initial_word, word)
+        tmp_sim = cos_sim(initial_word, word)
         if tmp_sim > max_sim[1]:
             max_sim = (word, tmp_sim)
     return max_sim
 
 
-def ngas(s_sen: list, t_sen: list):
+def ngas(src_sen: list, trg_sen: list):
     """
     Computes the non-symmetric greedy association similarity.
     I.e., the sum of all most similar alignments between the source and the target sentence,
     normalized by the length of the source sentence
-    :param s_sen: List of word embeddings of all words in the source sentence
-    :param t_sen: List of word embeddings of all words in the target sentence
+    :param src_sen: List of word embeddings of all words in the source sentence
+    :param trg_sen: List of word embeddings of all words in the target sentence
     :return: Non-symmetric greedy association similarity of the source and the target sentence
     """
     similarities_sum = 0.0
-    for word in s_sen:
-        similarities_sum += most_similar(word, t_sen)[1]
+    for word in src_sen:
+        similarities_sum += most_similar(word, trg_sen)[1]
     try:
-        return similarities_sum / len(s_sen)
+        return similarities_sum / len(src_sen)
     except ZeroDivisionError:
         return -1
 
 
-def gas(s_sen: list, t_sen: list):
+def gas(src_sen: list, trg_sen: list):
     """
     Computes the symmetric greedy association similarity.
     I.e., the sum of both non-symmetric association similarities (both directions), divided by 2
-    :param s_sen: List of word embeddings of all words in the source sentence
-    :param t_sen: List of word embeddings of all words in the target sentence
+    :param src_sen: List of word embeddings of all words in the source sentence
+    :param trg_sen: List of word embeddings of all words in the target sentence
     :return: Symmetric greedy association similarity of the source and the target sentence
     """
-    return 0.5*(ngas(s_sen, t_sen) + ngas(t_sen, s_sen))
+    return 0.5*(ngas(src_sen, trg_sen) + ngas(trg_sen, src_sen))
+
+
+# Dictionary of all vector_based features that can be extracted
+# alongside the corresponding function that needs to be executed for the given feature
+FEATURES = {
+    'cosine_similarity': [cos_sim, 'embedding']
+}
