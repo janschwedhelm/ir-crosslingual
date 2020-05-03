91b3019587166aad9891c03fd0e79a50d7ee184b
Rebecca Armbruster <rebeca.armbruster@sap.com>
2020-05-03 04:39:51 +0200

    Add paths to npy/pkl files of monolingual word embeddings and to further expert dictionaries

--- ir_crosslingual/utils/paths.py
+++ ir_crosslingual/utils/paths.py
@@ -2,15 +2,25 @@ data_path = '../../data/'
 
 model_path = '../main/models/'
 
-monolingual_embedding_paths = {
+monolingual_embedding_vec_paths = {
     'de': '{}fastText_mon_emb/wiki.de.vec'.format(data_path),
     'en': '{}fastText_mon_emb/wiki.en.vec'.format(data_path),
-    'fr': ''
+    'fr': '{}fastText_mon_emb/wiki.fr.vec'.format(data_path)
+}
+
+monolingual_embedding_paths = {
+    'de': '{}fastText_mon_emb/de/'.format(data_path),
+    'en': '{}fastText_mon_emb/en/'.format(data_path),
+    'fr': '{}fastText_mon_emb/fr/'.format(data_path)
 }
 
 expert_dictionaries = {
     'de-en': '{}expert_dictionaries/de-en/MUSE_de-en.0-5000.txt'.format(data_path),
-    'en-de': '{}expert_dictionaries/en-de/MUSE_en-de.0-5000.txt'.format(data_path)
+    'en-de': '{}expert_dictionaries/en-de/MUSE_en-de.0-5000.txt'.format(data_path),
+    'de-fr': '{}expert_dictionaries/de-fr/MUSE_de-fr.0-5000.txt'.format(data_path),
+    'fr-de': '{}expert_dictionaries/fr-de/MUSE_fr-de.0-5000.txt'.format(data_path),
+    'fr-en': '{}expert_dictionaries/fr-en/MUSE_fr-en.0-5000.txt'.format(data_path),
+    'en-fr': '{}expert_dictionaries/en-fr/MUSE_en-fr.0-5000.txt'.format(data_path)
 }
 
 sentence_dictionaries = {
