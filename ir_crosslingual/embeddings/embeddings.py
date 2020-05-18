350bf68281421cccf7f2fae78d03de7ffcf74486
Rebecca Armbruster <rebeca.armbruster@sap.com>
2020-05-18 03:31:43 +0200

    Add evaluation of multilingual word embeddings

--- ir_crosslingual/embeddings/embeddings.py
+++ ir_crosslingual/embeddings/embeddings.py
@@ -4,14 +4,19 @@ import pickle
 import numpy as np
 from numpy.linalg import svd
 from ir_crosslingual.utils import paths
+from sklearn.metrics.pairwise import cosine_similarity
 
 class WordEmbeddings:
 
     # Class attributes
     all_embeddings = dict()
+    test_embeddings = dict()
     projection_matrices = dict()
     seed_words = dict()
     seed_ids = dict()
+
+    test_seed_words = dict()
+    test_seed_ids = dict()
 
     seed_dicts = dict()
 
@@ -19,8 +24,9 @@ class WordEmbeddings:
     # we always load the same number of most frequent words.
     # Alternatively, this could also be passed to load_embeddings as a parameter
     LEARNING_METHODS = ['procrustes']  # todo: implement further methods
+    SIMILARITY_MEASURES = ['cosine']
 
-    def __init__(self, language):
+    def __init__(self, language, evaluation: bool = False):
         # Initiates new instance for given language. I.e., one instance for each language
         self.language = language
         self.embeddings = None
@@ -28,7 +34,10 @@ class WordEmbeddings:
         self.id2word = dict()
         self.aligned_subspace = dict()
 
-        WordEmbeddings.all_embeddings[language] = self
+        if evaluation:
+            WordEmbeddings.test_embeddings[language] = self
+        else:
+            WordEmbeddings.all_embeddings[language] = self
 
     def load_embeddings(self):
         """
@@ -44,6 +53,23 @@ class WordEmbeddings:
         # Create id2word dictionary
         self.id2word = {v: k for k, v in self.word2id.items()}
 
+    def load_vec_embeddings(self, n_max: int = 50000):
+        vectors = []
+        path = paths.monolingual_embedding_vec_paths[self.language]
+        word2id = dict()
+        with io.open(path, 'r', encoding='utf-8', newline='\n', errors='ignore') as file:
+            next(file)
+            for index, line in enumerate(file):
+                word, vec = line.rstrip().split(' ', 1)
+                vec = np.fromstring(vec, sep=' ')
+                vectors.append(vec)
+                word2id[word] = index
+                if len(word2id) == n_max:
+                    break
+        self.embeddings = np.vstack(vectors)
+        self.word2id = word2id
+        self.id2word = {v: k for k, v in self.word2id.items()}
+
     def align_monolingual_embeddings(self, languages: str, source: bool):
         """
         Align monolingual embeddings of self.language where self.language is either the source language
@@ -54,13 +80,21 @@ class WordEmbeddings:
         If False, self.language is the target language
         """
         seed_dict = WordEmbeddings.get_seed_ids(src_lang=languages[:2], trg_lang=languages[-2:])
-        self.aligned_subspace[languages[-2:] if source else languages[:2]] = self.embeddings[[tuples[not source] for tuples in seed_dict]]
-        print("---- INFO: Resulting subspace dimension: {}".format(self.aligned_subspace[languages[-2:] if source else languages[:2]].shape))
+        self.aligned_subspace[f'{languages[-2:]}_src' if source else f'{languages[:2]}_trg'] = self.embeddings[[tuples[not source] for tuples in seed_dict]]
+        print("---- INFO: Resulting subspace dimension: {}".format(self.aligned_subspace[f'{languages[-2:]}_src' if source else f'{languages[:2]}_trg'].shape))
 
     @classmethod
     def get_embeddings(cls, language: str):
         try:
             return cls.all_embeddings[language]
+        except KeyError:
+            print('---- ERROR: Embeddings for language {} do not exist yet.'.format(language))
+            return -1
+
+    @classmethod
+    def get_test_embeddings(cls, language: str):
+        try:
+            return cls.test_embeddings[language]
         except KeyError:
             print('---- ERROR: Embeddings for language {} do not exist yet.'.format(language))
             return -1
@@ -93,21 +127,22 @@ class WordEmbeddings:
             return -1
 
     @classmethod
-    def set_seed_dictionary(cls, src_lang: str, trg_lang: str):
+    def set_seed_dictionary(cls, src_lang: str, trg_lang: str, evaluation: bool = False):
         """
         Set seed dictionary for a given source/target language pair.
         Seed dictionary is then stored in WordEmbeddings.seed_words[language_pair]
         and WordEmbeddings.seed_ids[language_pair], for language_pair in in 'src-trg' format
         :param src_lang: Source language of the seed dictionary (short form, e.g. 'de')
         :param trg_lang: Target language of the seed dictionary (short form, e.g. 'de')
+        :param evaluation: If True, use test expert dictionary for evaluation of word embeddings
         """
         # TODO: Check that languages are in list
 
-        source = cls.get_embeddings(src_lang)
-        target = cls.get_embeddings(trg_lang)
+        source = cls.get_test_embeddings(src_lang) if evaluation else cls.get_embeddings(src_lang)
+        target = cls.get_test_embeddings(trg_lang) if evaluation else cls.get_embeddings(trg_lang)
         languages = '{}-{}'.format(src_lang, trg_lang)
 
-        expert_dict = paths.expert_dictionaries[languages]
+        expert_dict = paths.test_expert_dictionaries[languages] if evaluation else paths.expert_dictionaries[languages]
         index_pairs = []
         word_pairs = []
         misfit = 0
@@ -128,18 +163,6 @@ class WordEmbeddings:
                 print('---- INFO: Found {} valid translation pairs in expert dictionary.\n'
                       '---- INFO: {} other pairs contained at least one unknown word ({} in source language, {} in target language).'
                       .format(len(word_pairs), misfit, misfit_s, misfit_t))
-                # return index_pairs, word_pairs
-                cls.seed_words[languages] = word_pairs
-                cls.seed_ids[languages] = index_pairs
-                print(f'---- DONE: Seed dictionary extracted for the languages: {languages}')
-
-                # Create seed dictionary from cls.seed_words
-                cls.seed_dicts[languages] = dict()
-                for s in cls.seed_words[languages]:
-                    try:
-                        cls.seed_dicts[languages][s[0]].append(s[1])
-                    except KeyError:
-                        cls.seed_dicts[languages][s[0]] = [s[1]]
 
         elif isinstance(expert_dict, dict):
             for s_word, t_word in expert_dict.items():
@@ -151,11 +174,9 @@ class WordEmbeddings:
                     misfit_s += int(s_word not in source.word2id)
                     misfit_t += int(t_word not in target.word2id)
             print('---- INFO:Found {} valid translation pairs.\n'
-                  '---- INFO: {} other pairs contained at least one unknown word ({} in source language, {} in target language).'
+                  '---- INFO: {} other pairs contained at least one unknown word ({} in source language, '
+                  '{} in target language).'
                   .format(len(word_pairs), misfit, misfit_s, misfit_t))
-            # return index_pairs, word_pairs
-            cls.seed_words[languages] = word_pairs
-            cls.seed_ids[languages] = index_pairs
             print(f'---- DONE: Seed dictionary extracted for the languages: {languages}')
 
         else:
@@ -163,8 +184,24 @@ class WordEmbeddings:
             print('Invalid translation dictionary type. Text file or Python dictionary is required.')
             return -1
 
+        if evaluation:
+            cls.test_seed_words[languages] = word_pairs
+            cls.test_seed_ids[languages] = index_pairs
+        else:
+            cls.seed_words[languages] = word_pairs
+            cls.seed_ids[languages] = index_pairs
+
+            # Create seed dictionary from cls.seed_words
+            cls.seed_dicts[languages] = dict()
+            for s in cls.seed_words[languages]:
+                try:
+                    cls.seed_dicts[languages][s[0]].append(s[1])
+                except KeyError:
+                    cls.seed_dicts[languages][s[0]] = [s[1]]
+
     @classmethod
-    def learn_projection_matrix(cls, src_lang: str, trg_lang: str, method: str = 'procrustes', extract_seed: bool = True):
+    def learn_projection_matrix(cls, src_lang: str, trg_lang: str, method: str = 'procrustes',
+                                extract_seed: bool = True, align_subspaces: bool = True):
         """
         Learn projection matrices for a language pair in both directions.
         Projection matrices are then stored in WordEmbeddings.projection_matrices['src_lang-trg_lang']
@@ -172,10 +209,14 @@ class WordEmbeddings:
         :param src_lang: Source language (short form, e.g. 'de')
         :param trg_lang: Target language (short form, e.g. 'de')
         :param method: Learning method
-        :param extract_seed: Boolean variable to indicate whether the seed dictionary has be set or has been set yet.
+        :param extract_seed: Boolean variable to indicate whether the seed dictionary has to be set or has been set yet.
         If True, set seed dictionary in this function.
         If False, seed dictionary has already been set yet and is stored in WordEmbeddings.seed_words and
         WordEmbeddings.seed_ids already
+        :param align_subspaces: Boolean variable to indicate whether the subspaces have to be aligned or
+        have been aligned yet.
+        If True, align subspaces in this function.
+        If False, subspaces have already been aligned yet and are stored in WordEmbeddigns.aligned_subspace
         :return: Return projection matrices in both directions
         """
         for s_lang, t_lang in zip([src_lang, trg_lang], [trg_lang, src_lang]):
@@ -202,18 +243,60 @@ class WordEmbeddings:
                                 "embeddings are given as numpy arrays.")
 
             # Align subspaces
-            source.align_monolingual_embeddings(languages=languages, source=True)
-            target.align_monolingual_embeddings(languages=languages, source=False)
+            if align_subspaces:
+                source.align_monolingual_embeddings(languages=languages, source=True)
+                target.align_monolingual_embeddings(languages=languages, source=False)
 
             if method == 'procrustes':
-                U, s, Vt = svd(np.transpose(source.aligned_subspace[t_lang]) @ target.aligned_subspace[s_lang])
+                U, s, Vt = svd(np.transpose(source.aligned_subspace[f'{t_lang}_src']) @ target.aligned_subspace[f'{s_lang}_trg'])
                 W = U @ Vt
                 cls.projection_matrices[languages] = W
             print(f'---- DONE: Projection matrix learned from {s_lang} to {t_lang}')
         return cls.projection_matrices['{}-{}'.format(src_lang, trg_lang)],\
             cls.projection_matrices['{}-{}'.format(trg_lang, src_lang)]
 
-    # TODO: Add evaluation of multilingual embedding spaces
+    @classmethod
+    def evaluate_multilingual_embeddings(cls, src_lang: str, trg_lang: str, measure: str = 'cosine', k: int = 1):
+        if measure not in cls.SIMILARITY_MEASURES:
+            raise ValueError("---- ERROR: Similarity measure must be one of {}.".format(cls.SIMILARITY_MEASURES))
+
+        source = cls.get_test_embeddings(src_lang)
+        target = cls.get_test_embeddings(trg_lang)
+        languages = f'{src_lang}-{trg_lang}'
+
+        # Extract test seed dictionary as true translations
+        cls.set_seed_dictionary(src_lang=src_lang, trg_lang=trg_lang, evaluation=True)
+
+        src_test_indices = list({tup[0] for tup in cls.test_seed_ids[languages]})
+        trg_test_indices = list({tup[1] for tup in cls.test_seed_ids[languages]})
+        print(f'---- INFO: Aims to find correct translations between {len(src_test_indices)} source words '
+              f'and {len(trg_test_indices)} target words')
+
+        if len(src_test_indices) == 0:
+            raise ZeroDivisionError('---- ERROR: Cannot evaluate since no word pair of test dictionary is covered by'
+                                    'monolingual vocabularies')
+
+        W = cls.get_projection_matrix(src_lang=src_lang, trg_lang=trg_lang)
+        print(f'---- INFO: Start determination of top k={k} translations')
+        if measure == 'cosine':
+            topk_translations = {source.id2word[i]: [target.id2word[word_id] for word_id in
+                                                    cosine_similarity((source.embeddings[i] @ W).reshape(1,-1),
+                                                                      target.embeddings)[0].argsort()[-k:][::-1]]
+                                for i in src_test_indices}
+
+        print('---- INFO: Start identification of correct translations')
+        correct_translations = sum([bool(set(topk_translations[source.id2word[index]]).intersection(set([tup[1]
+                                    for tup in cls.test_seed_words[languages]
+                                    if tup[0] == source.id2word[index]]))) for index in src_test_indices])
+        accuracy = correct_translations / len(src_test_indices)
+
+        print(f'---- DONE: Accuracy with k={k}: {accuracy}')
+        print('-' * 60)
+        print(f'Examples of Top {k} translations:')
+        for word in ['recommend', 'geographical', 'developer']:
+            print(f'{word} -> {topk_translations[word]}')
+
+        return accuracy, topk_translations
 
 
 if __name__ == '__main__':
